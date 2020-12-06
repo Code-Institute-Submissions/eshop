dacd076d9b8533bdea958073ef44348308d1f2de
jyfoo213 <jyfoo213@gmail.com>
2020-12-06 13:06:48 +0000

    added listing, brand, seller db

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Listing
+from .models import Listing, Brand, Seller
 
 # Register your models here.
 admin.site.register(Listing)
+admin.site.register(Brand)
+admin.site.register(Seller)
--- listings/models.py
+++ listings/models.py
@@ -14,3 +14,19 @@ class Listing(models.Model):
     # toString function -- allow us to state the string rep of class
     def __str__(self):
         return self.title
+
+class Brand(models.Model):
+    # the fields (i.e. attributes of table)
+    name = models.CharField(blank=False, max_length=100)
+    website = models.CharField(blank=False, max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.website})"
+
+class Seller(models.Model):
+    seller_name = models.CharField(blank=False, max_length=255)
+    seller_email = models.CharField(blank=False, max_length=255)
+    seller_contact = models.CharField(blank=False, max_length=255)
+
+    def __str__(self):
+        return f"{self.seller_name} ({self.seller_email})"
