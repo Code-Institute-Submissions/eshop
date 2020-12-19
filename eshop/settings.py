1557e7708d901c4bacff93c8a32650542a872591
jyfoo213 <jyfoo213@gmail.com>
2020-12-19 11:11:30 +0000

    added master template

--- eshop/settings.py
+++ eshop/settings.py
@@ -61,7 +61,8 @@ ROOT_URLCONF = 'eshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
