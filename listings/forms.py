2e2a3ba142bbcf77ba4329d3b46fe0006fe40f70
jyfoo213 <jyfoo213@gmail.com>
2020-12-25 17:42:43 +0000

    form price input changed to cents

--- listings/forms.py
+++ listings/forms.py
@@ -3,7 +3,10 @@ from .models import Listing
 from cloudinary.forms import CloudinaryJsFileField
 
 class ListingForm(forms.ModelForm):
+    price = forms.IntegerField(label='Price (In Cents)')
     class Meta:
         model = Listing
-        fields = ('title', 'description', 'category', 'price', 'seller', 'cover')
+        fields = ['title', 'description', 'category', 'price', 'seller', 'cover']
     cover = CloudinaryJsFileField()
+
+
